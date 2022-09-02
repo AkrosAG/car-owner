a42395cc70cd67a0ee2ac277fd3c416b7f1f3976
Yordan Yordanov <yordyo@akros.ch>
2022-09-02 13:26:43 +0300

    manual k8s deployment added

--- infrastructure/k8s-local-deploy.sh
+++ infrastructure/k8s-local-deploy.sh
@@ -1,21 +1,26 @@
 echo "------------------------------------------------------------"
 echo "Building car owner service"
 echo "------------------------------------------------------------"
-#cd ../car-owner-service
-#mvn clean install
+cd ../car-owner-service
+mvn clean install
 
 echo ""
 echo "------------------------------------------------------------"
 echo "Building car owner ui"
 echo "------------------------------------------------------------"
-#cd ../car-owner-ui
-#npm install
-#npm run build
+cd ../car-owner-ui
+npm install
+npm run build
 
 echo ""
 echo "------------------------------------------------------------"
 echo "Building images"
 echo "------------------------------------------------------------"
 cd ../jkube-test
-#eval $(minikube docker-env)
-mvn clean k8s:build k8s:resource k8s:push k8s:apply
+mvn clean k8s:build k8s:push
+
+echo ""
+echo "------------------------------------------------------------"
+echo "Applying k8s config"
+echo "------------------------------------------------------------"
+kubectl apply -f ./src/k8s
