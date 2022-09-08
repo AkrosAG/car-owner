14e23bbb579e3aa7e1be47954c2fd9da78d3937b
Ivan Simeonov <ivan.simeonov@frox.ch>
2022-09-08 11:13:32 +0300

    Testing

--- car-owner-ui/src/app/app.module.ts
+++ car-owner-ui/src/app/app.module.ts
@@ -7,18 +7,34 @@ import { OwnersComponent } from './owners/owners.component';
 import { OwnerDetailsComponent } from './owners/owner-details/owner-details.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     AppComponent,
     OwnersComponent,
-    OwnerDetailsComponent
+    OwnerDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- car-owner-ui/src/app/shared/service/owner.service.ts
+++ car-owner-ui/src/app/shared/service/owner.service.ts
@@ -2,21 +2,26 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import {Owner} from '../entities/owner.type';
+import {environment} from "../../../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnerService {
-  constructor(private http: HttpClient) {}
+
+
+  constructor(private http: HttpClient) {
+  }
 
   getOwnerList(): Observable<any> {
-    return this.http.get('http://localhost:8080/owner/list').pipe(map(response => {
+    // return this.http.get('http://127.0.0.1:64891/owner/list').pipe(map(response => {
+    return this.http.get(`http://${environment.backend_address}:8080/owner/list`).pipe(map(response => {
       return response as Owner[];
     }));
   }
 
   getOwner(id: number): Observable<any> {
-    return this.http.get('http://localhost:8080/owner/' + id).pipe(map(response => {
+    return this.http.get('http://127.0.0.1:64891/owner/' + id).pipe(map(response => {
       return response as Owner;
     }));
   }
