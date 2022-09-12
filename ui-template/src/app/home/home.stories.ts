7c03bf0f1f0ca7829a6b4024539d11d8372b0bcf
Martin Dobrev <martin.dobrev@akros.ch>
2022-09-12 23:45:31 +0300

    home story example added, global styles CANNOT be loaded

---
+++ ui-template/src/app/home/home.stories.ts
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import {HomeComponent} from './home.component';
+
+export default {
+  title: 'UI-Template/HomeComponent',
+  component: HomeComponent,
+} as Meta;
+
+const Template: Story<HomeComponent> = () => ({
+
+})
+
+export const General = Template.bind({});
