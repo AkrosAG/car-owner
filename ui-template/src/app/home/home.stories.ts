f43f8446f41fb446a61554cf370798643849d2d0
Martin Dobrev <martin.dobrev@akros.ch>
2022-09-13 09:39:38 +0300

    Styles correctly integrated into stories

--- ui-template/src/app/home/home.stories.ts
+++ ui-template/src/app/home/home.stories.ts
@@ -1,9 +1,12 @@
+import {moduleMetadata} from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import {HomeComponent} from './home.component';
+import {MatButtonModule} from '@angular/material/button';
 
 export default {
   title: 'UI-Template/HomeComponent',
   component: HomeComponent,
+  decorators: [moduleMetadata({imports: [MatButtonModule]})]
 } as Meta;
 
 const Template: Story<HomeComponent> = () => ({
